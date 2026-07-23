114e778b1edd77f225c3e09626ee9336ef6a4787
jake-warburton <jakewarburton6@gmail.com>
2026-07-23 21:09:11 +0100

    stage 1: app shell with MUI baseline and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Container, CssBaseline, Typography } from "@mui/material";
+
 function App() {
-  return <div>Hello World.</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Container>
+        <Typography variant="h1" component="h1">
+          Hello World.
+        </Typography>
+      </Container>
+    </>
+  );
 }
 
 export default App;
