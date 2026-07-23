3ec205c02ec59b4327eae612f0d4e2509c16cfcb
jake-warburton <jakewarburton6@gmail.com>
2026-07-24 00:23:29 +0100

    stage 1: refactor countrySlugToLabel to map and join

--- src/components/QuestionnaireForm/countrySlugToLabel.ts
+++ src/components/QuestionnaireForm/countrySlugToLabel.ts
@@ -1,11 +1,7 @@
 export const countrySlugToLabel = (slug: string) => {
   const words = slug.split("-");
 
-  const capitalizedWords: string[] = [];
-
-  words.forEach((word) =>
-    capitalizedWords.push(`${word[0].toUpperCase()}${word.slice(1)}`),
-  );
-
-  return capitalizedWords.join(" ");
+  return words
+    .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
+    .join(" ");
 };
