abee4e5faf99fa50a2a39997d1841a134d9cff66
jake-warburton <jakewarburton6@gmail.com>
2026-07-24 17:48:58 +0100

    feat: green - roll unavailable days back onto the previous collectable day

--- src/domain/distributePickups/distributePickups.ts
+++ src/domain/distributePickups/distributePickups.ts
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import type { DayOfWeek } from "../types";
 
 interface DistributePickupsArgs {
@@ -12,9 +13,30 @@ export const distributePickups = ({
   doses,
   availableDays,
   bankHolidays,
-}: DistributePickupsArgs) =>
-  dates.map((date, i) => ({
-    date,
-    dose: doses[i],
-    pickup: doses[i],
-  }));
+}: DistributePickupsArgs) => {
+  const pickupsArray = [];
+
+  let accumulatedDosage = 0;
+
+  for (let i = dates.length - 1; i > -1; i -= 1) {
+    accumulatedDosage += doses[i];
+
+    //  Get current date's day of week
+    const currentDayOfWeek = format(dates[i], "eeee") as DayOfWeek;
+
+    const dosageToPickupToday = availableDays.includes(currentDayOfWeek)
+      ? accumulatedDosage
+      : 0;
+
+    //  Service user picks up today. Reset accumulator back to 0 for tomorrow
+    if (dosageToPickupToday > 0) accumulatedDosage = 0;
+
+    pickupsArray.push({
+      date: dates[i],
+      dose: doses[i],
+      pickup: dosageToPickupToday,
+    });
+  }
+
+  return pickupsArray.reverse();
+};
