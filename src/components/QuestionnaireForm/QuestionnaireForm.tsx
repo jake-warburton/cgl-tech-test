89a130430713b6e8d3a7dba0ac6a6531ef00682e
jake-warburton <jakewarburton6@gmail.com>
2026-07-24 01:08:24 +0100

    stage 1: QuestionnaireForm green with availability checkboxes

--- src/components/QuestionnaireForm/QuestionnaireForm.tsx
+++ src/components/QuestionnaireForm/QuestionnaireForm.tsx
@@ -1,8 +1,27 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 
 import bankHolidays from "../../data/bank-holidays.json";
 import { countrySlugToLabel } from "./countrySlugToLabel";
 import { useState } from "react";
+
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 interface QuestionnaireFormProps {
   onSubmit: () => void;
@@ -12,20 +31,33 @@ export const QuestionnaireForm = ({}: QuestionnaireFormProps) => {
   const [country, setCountry] = useState("england-and-wales");
 
   return (
-    <FormControl>
-      <InputLabel id="country-label">Country</InputLabel>
-      <Select
-        labelId="country-label"
-        label="Country"
-        value={country}
-        onChange={(event) => setCountry(event.target.value)}
-      >
-        {Object.keys(bankHolidays).map((slug) => (
-          <MenuItem key={slug} value={slug}>
-            {countrySlugToLabel(slug)}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+    <>
+      <FormControl>
+        <InputLabel id="country-label">Country</InputLabel>
+        <Select
+          labelId="country-label"
+          label="Country"
+          value={country}
+          onChange={(event) => setCountry(event.target.value)}
+        >
+          {Object.keys(bankHolidays).map((slug) => (
+            <MenuItem key={slug} value={slug}>
+              {countrySlugToLabel(slug)}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      <FormControl component="fieldset">
+        <FormLabel component="legend">
+          What days of the week is the service user generally available?
+        </FormLabel>
+        <FormGroup>
+          {daysOfWeek.map((day) => (
+            <FormControlLabel key={day} label={day} control={<Checkbox />} />
+          ))}
+        </FormGroup>
+      </FormControl>
+    </>
   );
 };
